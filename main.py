1036eb878e535f1f98bb2ee14a8e01bb95ce845a
Cole Green <17110935+nox7@users.noreply.github.com>
2024-07-25 16:47:36 -0500

    Implement main loop, main(), menu, encode/decode

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def encode(password):
     """
     Mutates the password argument by iterating over every numeric character and adding 3 to that digit.
@@ -19,8 +22,35 @@ def decode(password):
     return x
 
 
+def show_menu_and_get_input():
+    choice = 0
+    while choice <= 0 or choice > 3:
+        print("Menu")
+        print("----------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit", end="\n\n")
+        try:
+            choice = int(input("Please enter an option: "))
+        except ValueError:
+            choice = 0
+            print("Enter a valid menu choice.", end="\n\n")
+
+    return choice
+
+
 def main():
-    pass
+    current_password = ""
+    while True:
+        choice = show_menu_and_get_input()
+        if choice == 1:
+            password_entered = input("Please enter the password to encode: ")
+            current_password = encode(password_entered)
+            print("Your password has been encoded and stored!", end="\n\n")
+        elif choice == 2:
+            print(f"The encoded password is {current_password}, and the original password is {decode(current_password)}.")
+        elif choice == 3:
+            break
 
 
 if __name__ == "__main__":
