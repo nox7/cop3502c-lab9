bc91058f555d0ce772d26d41627435c4b5bd9c3e
Cole Green <17110935+nox7@users.noreply.github.com>
2024-07-25 16:40:57 -0500

    Return string and not int

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def encode(password):
     Mutates the password argument by iterating over every numeric character and adding 3 to that digit.
     Returns the result after mutation.
     """
-    return int("".join(str(int(digit)+3) for digit in password))
+    return "".join(str(int(digit)+3) for digit in password)
 
 
 def decode(password):
